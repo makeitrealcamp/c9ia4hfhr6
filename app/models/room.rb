830224737b4b5205cd367c9b26c54994e33abfb5
jhon11390 <meljhon2@gmail.com>
2020-03-07 13:56:49 -0500

    Se inserta mensajes de validacion y error

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,5 @@
 class Room < ApplicationRecord
-    validates :title, :description, :beds, :guests, :image_url, :presence =>true
+    validates :title, :description, :beds, :guests, :image_url, presence: true
     validates :description, length: { maximum: 400}
-    validates :beds, numericality: true
-    validates :beds, numericality: { only_integer: true }
+    validates :beds, :guests, numericality: { only_integer: true }
 end

da10c9180e079c660bebb74a03c37a8721fea0ae
jhon11390 <meljhon2@gmail.com>
2020-03-07 12:21:40 -0500

    se agrega validaciones al modelo Room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,6 @@
 class Room < ApplicationRecord
+    validates :title, :description, :beds, :guests, :image_url, :presence =>true
+    validates :description, length: { maximum: 400}
+    validates :beds, numericality: true
+    validates :beds, numericality: { only_integer: true }
 end

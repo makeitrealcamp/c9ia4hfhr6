6b7b7898e9ec8c58fa58acb2177609b6f706d571
dacardenasa <diego.cardenasaleg@gmail.com>
2020-03-20 11:25:18 -0500

    Validate modal fields and show errors in views create and edit

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,9 @@
 class Room < ApplicationRecord
+  # Validate fields  aren't blank or nill
+  validates_presence_of :title, :description, :beds, :guests, :image_url
+  # Validate length description input to 400 characters
+  validates :description, length: { maximum: 400 }
+  #  Validate fields are numbers
+  validates :beds, :guests, numericality: true
+  validates :beds, :guests, numericality: { only_integer: true }
 end

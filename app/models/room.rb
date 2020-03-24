c9997885de10db47ebb41ddb08d2942696c8c3d6
dacardenasa <diego.cardenasaleg@gmail.com>
2020-03-24 15:52:42 -0500

    Update validate price_per_night off in model room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   # Validate fields  aren't blank or nill
-  validates_presence_of :title, :description, :beds, :guests, :price_per_night, :image_url
+  validates_presence_of :title, :description, :beds, :guests, :image_url
   # Validate length description input to 400 characters
   validates :description, length: { maximum: 400 }
   #  Validate fields are numbers

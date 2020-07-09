e3b2b6072449fea2d4e9de8e2723d7fbc5e375d9
pakman <pakman@DESKTOP-J81E3KF.localdomain>
2020-07-08 23:26:56 -0500

    added the validations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,5 @@
 class Room < ApplicationRecord
+    validates :title, :description, :beds, :guests, :image_url, presence: true
+    validates :description, length: { maximum: 400 }
+    validates :beds, :guests, numericality: { only_integer: true }
 end

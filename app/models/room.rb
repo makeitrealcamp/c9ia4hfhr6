7a0e563999319dc6244630879786ded4cb5e365e
pakman <pakman@DESKTOP-J81E3KF.localdomain>
2020-07-10 16:37:01 -0500

    Add a new column

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ApplicationRecord
-    validates :title, :description, :beds, :guests, :image_url, presence: true
+    validates :title, :description, :beds, :guests, :image_url, :price_per_night, presence: true
     validates :description, length: { maximum: 400 }
-    validates :beds, :guests, numericality: { only_integer: true }
+    validates :beds, :guests, :price_per_night, numericality: { only_integer: true }
+   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_214436) do
+ActiveRecord::Schema.define(version: 2020_07_10_210033) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_02_06_214436) do
     t.string "image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "price_per_night"
   end
 
 end

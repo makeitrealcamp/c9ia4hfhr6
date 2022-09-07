12f1341fab8eaf6f7a96de645b3c32832f41297b
GUIPARO <guisela2728@gmail.com>
2022-09-07 09:36:22 -0500

    add method update room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,11 +9,26 @@ class RoomsController < ApplicationController
 
   def create
     @room = Room.new(room_params)
+
     if @room.save
       redirect_to rooms_path
     else
       @errors = @room.errors.full_messages
       render :new
+    end
+  end
+
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+
+    if @room.update(room_params)
+      redirect_to rooms_path
+    else
+      render :edit
     end
   end
 
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
-  validates :title,:description,:beds,:guests,:image_url, :price_per_night presence: true
+  validates :title,:description,:beds,:guests,:image_url, :price_per_night, presence: true
   validates :description, length: { maximum: 400 }
   validates :beds,:guests, numericality: { only_integer: true }
 end
